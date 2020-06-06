fe58a16ead025c01d1d0d4db7aba0b0c52eeb339
Ashuwin <beegsashuwin2@gmail.com>
2020-06-06 18:33:17 +0530

    Latex figure deleted

--- linalg_triangle/codes/tri_area_PQR.py
+++ linalg_triangle/codes/tri_area_PQR.py
@@ -8,11 +8,6 @@ P = np.array([-5,-1])
 Q = np.array([3,-5])
 R = np.array([5,2])
 
-p = np.linalg.norm(Q-R)
-q = np.linalg.norm(R-P)
-r = np.linalg.norm(P-Q)
-
-s = (p+q+r)/2
-area = np.sqrt(s*(s-p)*(s-q)*(s-r))
+area = (1/2)*np.linalg.norm(np.cross(Q-P,R-P))
 
 print("Area of triangle PQR: ",area)
